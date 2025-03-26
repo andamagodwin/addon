fa0439a7d615db70d7ccfec25be392d95704f272
andamagodwin <andamagodwinezra@gmail.com>
2025-03-26 17:39:04 +0300

    updated the main stage url

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 
 export const CLOUD_PROJECT_NUMBER: string = "44770182672";
-export const MAIN_STAGE_URL: string = "https://googleworkspace.github.io/meet/hello-world-next-js/mainstage"
+export const MAIN_STAGE_URL: string = "https://lingomeet-addon--ingomeet-addon.europe-west4.hosted.app/mainstage"
